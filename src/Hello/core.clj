6f7c5cebe21d860f12d92c43aefbe2d7d3f0381c
Sławomir Bogutyn <sbogutyn@gmail.com>
2010-12-17 00:15:02 +0100

    adding functions

--- src/Hello/core.clj
+++ src/Hello/core.clj
@@ -1,4 +1,31 @@
 (ns Hello.core
     (:gen-class))
+  
+(defn to-upper
+  "Returns string parameter in upper case."
+  [string]
+  (.toUpperCase string))
+  
+(defn length
+  "Returns string length"
+  [string]
+  (.length string))
+
+(defn square
+  "Squares a number, with side effects."
+  [x]
+  (println "Squaring" x)
+  (println "The return value will be" (* x x)))
+  
+(defn arg-switch
+  "Applies the supplied function to the arguments in both possible orders."
+  [fun arg1 arg2]
+  (list (fun arg1 arg2) (fun arg2 arg1)))
+  
 (defn -main [& args]
-  (println "Hello World!"))
+  (def napis "Hello World!")
+  (println napis)
+  (println "Dlugosc" napis "to" (length napis))
+  (square 4)
+  (println "(arg-switch / 2 3) to" (arg-switch / 2 3)
+  (println "(arg-switch str \"Hello\" \"World\")" (arg-switch str "Hello" "World"))))
